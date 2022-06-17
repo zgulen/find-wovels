a5a012cfc21f28fb98042eba6f9d6bd655c6f4ce
zgulen <gulenzubeyir@gmail.com>
2022-06-17 21:34:06 +0300

    vowels done

---
+++ app.js
@@ -0,0 +1,17 @@
+const form = document.querySelector(".form")
+const textArea = document.getElementById("text")
+const notification = document.querySelector(".notification")
+const button = document.querySelector("button")
+const array =["a","e","ı","i","o","ü","ö","u"]
+form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    let counter = 0
+    for (let i = 0; i < array.length; i++) {
+            textArea.value.split("").map((e) =>{
+                if (e == array[i]){
+                    counter++
+                }
+            })
+        }
+        notification.textContent = `There are/is ${counter} vowels in "${textArea.value}"`
+})
